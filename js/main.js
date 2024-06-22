ed87da54894324d375e8bec9ac8c6fd131c556d8
Arnav <100010741+arnavravinder@users.noreply.github.com>
2024-06-22 18:31:02 +0530

    update css and js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active'));
+
